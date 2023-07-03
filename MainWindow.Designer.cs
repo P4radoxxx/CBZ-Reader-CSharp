cb6dbd542cde351386714ef345d9b94cbef248d8
P4radoxxx <131610338+P4radoxxx@users.noreply.github.com>
2023-07-03 09:22:50 +0200

    Add files via upload

---
+++ MainWindow.Designer.cs
@@ -0,0 +1,167 @@
+﻿namespace TFTIC_P4radoxCBZReader
+{
+    partial class MainWindow
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.menuToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.ouvrirToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.quitterToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.buttonSuivant = new System.Windows.Forms.Button();
+            this.buttonPrecedent = new System.Windows.Forms.Button();
+            this.DemoImages = new System.Windows.Forms.PictureBox();
+            this.tFTICToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.menuStrip1.SuspendLayout();
+            this.panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.DemoImages)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.menuToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(903, 24);
+            this.menuStrip1.TabIndex = 0;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // menuToolStripMenuItem
+            // 
+            this.menuToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.ouvrirToolStripMenuItem,
+            this.quitterToolStripMenuItem,
+            this.tFTICToolStripMenuItem});
+            this.menuToolStripMenuItem.Name = "menuToolStripMenuItem";
+            this.menuToolStripMenuItem.Size = new System.Drawing.Size(50, 20);
+            this.menuToolStripMenuItem.Text = "Menu";
+            // 
+            // ouvrirToolStripMenuItem
+            // 
+            this.ouvrirToolStripMenuItem.Name = "ouvrirToolStripMenuItem";
+            this.ouvrirToolStripMenuItem.ShortcutKeys = System.Windows.Forms.Keys.F1;
+            this.ouvrirToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.ouvrirToolStripMenuItem.Text = "Ouvrir CBZ";
+            this.ouvrirToolStripMenuItem.Click += new System.EventHandler(this.ouvrirToolStripMenuItem_Click);
+            // 
+            // quitterToolStripMenuItem
+            // 
+            this.quitterToolStripMenuItem.Name = "quitterToolStripMenuItem";
+            this.quitterToolStripMenuItem.ShortcutKeys = System.Windows.Forms.Keys.F11;
+            this.quitterToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.quitterToolStripMenuItem.Text = "Quitter";
+            this.quitterToolStripMenuItem.Click += new System.EventHandler(this.quitterToolStripMenuItem_Click);
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.buttonSuivant);
+            this.panel1.Controls.Add(this.buttonPrecedent);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.panel1.Location = new System.Drawing.Point(0, 571);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(903, 100);
+            this.panel1.TabIndex = 1;
+            // 
+            // buttonSuivant
+            // 
+            this.buttonSuivant.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.buttonSuivant.Location = new System.Drawing.Point(800, 19);
+            this.buttonSuivant.Name = "buttonSuivant";
+            this.buttonSuivant.Size = new System.Drawing.Size(75, 69);
+            this.buttonSuivant.TabIndex = 1;
+            this.buttonSuivant.Text = ">";
+            this.buttonSuivant.UseVisualStyleBackColor = true;
+            this.buttonSuivant.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // buttonPrecedent
+            // 
+            this.buttonPrecedent.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.buttonPrecedent.Location = new System.Drawing.Point(25, 19);
+            this.buttonPrecedent.Name = "buttonPrecedent";
+            this.buttonPrecedent.Size = new System.Drawing.Size(75, 69);
+            this.buttonPrecedent.TabIndex = 0;
+            this.buttonPrecedent.Text = "<";
+            this.buttonPrecedent.UseVisualStyleBackColor = true;
+            this.buttonPrecedent.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // DemoImages
+            // 
+            this.DemoImages.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.DemoImages.Location = new System.Drawing.Point(0, 24);
+            this.DemoImages.Name = "DemoImages";
+            this.DemoImages.Size = new System.Drawing.Size(903, 547);
+            this.DemoImages.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.DemoImages.TabIndex = 2;
+            this.DemoImages.TabStop = false;
+            this.DemoImages.Click += new System.EventHandler(this.tFTICToolStripMenuItem_Click);
+            // 
+            // tFTICToolStripMenuItem
+            // 
+            this.tFTICToolStripMenuItem.Name = "tFTICToolStripMenuItem";
+            this.tFTICToolStripMenuItem.ShortcutKeys = System.Windows.Forms.Keys.F12;
+            this.tFTICToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.tFTICToolStripMenuItem.Text = "TFTIC";
+            this.tFTICToolStripMenuItem.Click += new System.EventHandler(this.tFTICToolStripMenuItem_Click);
+            // 
+            // MainWindow
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(903, 671);
+            this.Controls.Add(this.DemoImages);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "MainWindow";
+            this.Text = "P4radox CBZ Reader";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.panel1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.DemoImages)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem menuToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem ouvrirToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem quitterToolStripMenuItem;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Button buttonSuivant;
+        private System.Windows.Forms.Button buttonPrecedent;
+        private System.ComponentModel.BackgroundWorker backgroundWorker1;
+        private System.Windows.Forms.PictureBox DemoImages;
+        private System.Windows.Forms.ToolStripMenuItem tFTICToolStripMenuItem;
+    }
+}
+
---
+++ MainWindow.cs
@@ -0,0 +1,198 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO.Compression;
+using System.Reflection;
+
+namespace TFTIC_P4radoxCBZReader
+{
+    public partial class MainWindow : Form
+    {
+
+        public MainWindow()
+        {
+            
+            InitializeComponent();
+            Application.ApplicationExit += new EventHandler(this.QuitParInterfaceFenetre);
+        }
+
+        // Liste des chemins des images extraites, seront recupérés dans les fonctions d'affichages
+        public List<string> AdresseImgExtraites = new List<string>();
+        int IndexListImg;
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+
+            // Code à executer au chargement de la MainWindow(), probablement rien...
+
+        }
+
+
+        
+        
+        
+        private void ouvrirToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            string dossierTemp = Path.GetTempPath();
+            string cheminDossierTemp = Path.Combine(dossierTemp, "TempCBZFolder"); // Appdata/local/temp/TempCBZFolder
+
+            // Crée un repertoire temporaire pour extraire nos images
+            Directory.CreateDirectory(cheminDossierTemp);
+
+            // Check si le rep existe
+            if (!Directory.Exists(cheminDossierTemp))
+            {
+                MessageBox.Show("Impossible de créer un dossier temporaire :( ");
+            }
+
+            OpenFileDialog openFileDialog = new OpenFileDialog();
+            openFileDialog.Title = "Choix du fichier CBZ";
+            openFileDialog.Filter = "Fichiers CBZ|*.cbz";
+
+
+
+
+            if (openFileDialog.ShowDialog() == DialogResult.OK)
+            {
+                string chemin = openFileDialog.FileName;
+
+                try
+                {
+                    System.IO.Compression.ZipFile.ExtractToDirectory(chemin, cheminDossierTemp);
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine("Erreur pendant l'extraction de l'archive: " + ex.Message);
+                }
+
+                DirectoryInfo directoryInfo = new DirectoryInfo(cheminDossierTemp);
+
+                // Traverse tous les répertoires  au cas ou les fichiers seraient dans un sous rep.
+                FileInfo[] CbzImage = directoryInfo.GetFiles("*", SearchOption.AllDirectories);
+
+                foreach (FileInfo CbzImages in CbzImage)
+                {
+                    string extension = CbzImages.Extension.ToLower();
+                    if (extension == ".jpg" || extension == ".jpeg" || extension == ".png" || extension == ".gif")
+                    {
+                        AdresseImgExtraites.Add(CbzImages.FullName);
+                        
+                    }
+                }
+
+                // Debug
+                //Console.WriteLine($"{AdresseImgExtraites.Count} Images chargées dans la liste");
+
+                MessageBox.Show(" Archive extraite ! Appuyez sur la flêche suivante pour commencer la lecture :3");
+            }
+        }
+
+
+
+
+        private void quitterToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+
+            string dossierTemp = Path.GetTempPath();
+            string cheminDossierTemp = Path.Combine(dossierTemp, "TempCBZFolder");
+
+            // Vide la PictureBox
+            DemoImages.Image?.Dispose();
+            DemoImages.Image = null;
+
+            // JUste pour s'assurer que la PB soit bien vide avant de supprimer le dossier temp et de quitter l'app.
+            System.Threading.Thread.Sleep(2000);
+
+            if (Directory.Exists(cheminDossierTemp))
+            {
+                Directory.Delete(cheminDossierTemp, true);
+            }
+            Application.Exit();
+   
+        }
+
+
+
+
+
+
+
+        // Fonction affichage image précedente
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (IndexListImg > 0)
+            {
+                IndexListImg--;
+                DemoImages.Image?.Dispose();
+                DemoImages.Image = Image.FromFile(AdresseImgExtraites[IndexListImg]);
+            }
+            else
+            {
+                MessageBox.Show("Pas d'image avant celle la!");
+            }
+        }
+
+        // Image suivante
+        private void button2_Click(object sender, EventArgs e)
+        {
+            if (IndexListImg < AdresseImgExtraites.Count - 1)
+            {
+                IndexListImg++;
+                DemoImages.Image?.Dispose();
+                DemoImages.Image = Image.FromFile(AdresseImgExtraites[IndexListImg]);
+            }
+            else
+            {
+                MessageBox.Show("Plus d'images après celle la !");
+            }
+        }
+
+
+
+
+        // Affichage box TFTIC
+        private void tFTICToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+
+            MessageBox.Show("Hello TFTIC from Cyril 'P4radox' Bouvier");
+
+        }
+
+
+
+        // Trigger d'event winform pour l'objet PictureBox, useless, la PB ne sera utilisée que comme support d'affichage à travers ses fonctions.
+        private void DemoImages_Click(object sender, EventArgs e)
+        {
+
+        }
+
+
+        // Delete le dossier temporaire quand l'app est quittée depuis l'interface de la fenêtre, pasted from above...
+        private void QuitParInterfaceFenetre(object sender, EventArgs e)
+        {
+            string dossierTemp = Path.GetTempPath();
+            string cheminDossierTemp = Path.Combine(dossierTemp, "TempCBZFolder");
+
+            
+            DemoImages.Image?.Dispose();
+            DemoImages.Image = null;
+
+            
+            System.Threading.Thread.Sleep(2000);
+
+            if (Directory.Exists(cheminDossierTemp))
+            {
+                Directory.Delete(cheminDossierTemp, true);
+            }
+        }
+
+    }
+}
